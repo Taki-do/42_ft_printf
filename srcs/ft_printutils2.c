710f03f82d7f6e90b80133d752b519f8a6c91149
Taki-do <malbecktao@gmail.com>
2024-11-14 15:50:47 +0100

    Mandatory version

--- srcs/ft_printutils2.c
+++ srcs/ft_printutils2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_check_format.c                                  :+:      :+:    :+:   */
+/*   ft_printutils2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taomalbe <taomalbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:46:21 by taomalbe          #+#    #+#             */
-/*   Updated: 2024/11/07 15:20:27 by taomalbe         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:45:41 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,26 +50,3 @@ int	ft_hexa_len(unsigned int dec, int ul)
 	stock[i] = '\0';
 	return (ft_putstr_len(ft_strrev(stock)));
 }
-//If '%', while there valid flags, check if this can means someting ?
-// And after all dispatch
-
-//- Is '0', check if there is a number after, do number - sizeof(%d)
-// to know how may '0' needed
-//to be print starting from left. 'd | i | u | x | X'.
-
-//- '-', get the content to the left, add spaces if a number is
-// specified after '"%-5%d", 42'
-//will be : "42   ". 'all'.
-
-//- '.', precision, it define minimal number of caracters to print
-//, like for '"%.3d", 7' =
-//'007', or for '"%.3s", "hello"' = 'hel'. 'd | i | u | x | X'.
-
-//- '#', for the hexadecimal content, display '0x' in front. 'x | X'.
-
-//- '+', force to print the sign, '+' or '-'. 'd | i'.
-
-//- ' ', put a space before the integers, but if there is a sign it
-// takes priority. 'd | i'.
-
-//- Flag priority, '- > 0', '+- > ' '', '.0 > 05', 
